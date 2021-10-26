1330952fd8f2a8777196960b2c11d72d2b5d9ae2
Volga <persikmacnev@gmail.com>
2021-10-27 00:09:23 +0300

    26.10.21

---
+++ Controllers/CRUD_ArtExhibitiontQuestionController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Art_exhibition.Controllers
+{
+    [ApiController]
+    [Route("/question")]
+    public class QuestionController : ControllerBase
+    {
+        [HttpPut("Create")]
+        public string Create(string str)
+        {
+            return str;// Метод создания
+        }
+
+        [HttpGet("Read")]
+        public string Read(string str)
+        {
+            return str;// Метод чтения
+        }
+
+        [HttpPatch("Update")]
+        public string Update(string str)
+        {
+            return str;// Метод обновления
+        }
+
+        [HttpDelete("Delete")]
+        public string Delete(string str)
+        {
+            return str;// Метод удаления
+        }
+    }
+}
